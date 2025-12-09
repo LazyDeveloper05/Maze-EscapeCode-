5acd30204747871ee716861a99e1cdb723382556
LazyDeveloper05 <nelbertmerto8@gmail.com>
2025-12-09 20:01:59 +0800

    Add files via upload

---
+++ door_10.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var entered = false
+
+# Called when a body enters the Area2D
+func _on_body_entered(_body: Node2D) -> void:
+	entered = true
+
+# Called when a body exits the Area2D
+func _on_body_exited(_body: Node2D) -> void:
+	entered = false
+
+func _process(_delta: float) -> void:
+	if entered and Input.is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file("res://scene/Final_Map.tscn")
